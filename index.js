94421c3e25af03a9e2926d15f3d469160d15c32e
meron847 <belachewmeron15@gmail.com>
2025-11-29 11:58:59 +0000

    add event listner

---
+++ index.js
@@ -0,0 +1,23 @@
+const letters = document.querySelectorAll('box')
+async function init() {
+
+
+    document.addEventListener('keydown', function handleKeyPress (event){
+        const action = event.key;
+
+        console.log(action);
+
+        if (action === 'Enter'){
+            commit();
+        }else if (action === 'Backspace') {
+            backspace();
+        }else if (isLetter(action)) {
+            addLetter(action.toUpperCase())
+        }else{
+            //do nothing
+        }
+    }); 
+
+    
+}
+init();
