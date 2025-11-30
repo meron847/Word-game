de0556dc61138b57dd2b42d4423d5a2177c71dd0
meron847 <belachewmeron15@gmail.com>
2025-11-30 15:11:13 +0000

    add backspace

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 
 const ANSWER_LENGTH = 5;
-
 async function init() {
     const letters = document.querySelectorAll(".cell");
 
   let currentGuess ='';
-  let currentRow = '0';
+  let currentRow = 0;
     function addLetter (letter) {
         if (currentGuess.length < ANSWER_LENGTH) {
             //add letters to the end
             currentGuess += letter;
-        }else {
+         }else {
             //replace the last letter
             currentGuess = currentGuess.substring(0, currentGuess.length -1)+ letter;
-        }
-        //show the text in screen 
+            
+         }
+         //show the text in screen 
         
+       letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
+        
+     }
 
-        letters[currentGuess.length - 1].innerText = letter;
-    }
+     function backspace() {
+     if (currentGuess.length > 0) {
+        // erase letter from the grid
+        const index = ANSWER_LENGTH * currentRow + (currentGuess.length - 1);
+        letters[index].innerText = "";
 
-    document.addEventListener('keydown', function handleKeyPress (event){
+        // erase last letter
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+      }
+     }
+
+
+     async function commit() {
+        if (currentGuess.length !== ANSWER_LENGTH) {
+            //do nothing 
+            return;
+        }
+        //ToDo did they win or lose?
+        currentRow++;
+        currentGuess = '';
+     }
+
+     document.addEventListener('keydown', function handleKeyPress (event){
         const action = event.key;
 
         console.log(action);
@@ -34,9 +56,9 @@ async function init() {
         }else{
             //do nothing
         }
-    }); 
-}
-function isLetter(letter) {
+     }); 
+ }
+ function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
-}
+ }
 init();
