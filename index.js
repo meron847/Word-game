485d92bde67f569c3250b5c409924572acbd49b4
meron847 <belachewmeron15@gmail.com>
2025-12-01 21:19:56 +0000

    add function called add Letter

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
-
 const ANSWER_LENGTH = 5;
+
+const loadingDiv = document.querySelector(".loading");
+
+
 async function init() {
     const letters = document.querySelectorAll(".cell");
 
   let currentGuess ='';
   let currentRow = 0;
-    function addLetter (letter) {
-        if (currentGuess.length < ANSWER_LENGTH) {
+
+ function backspace() {
+  if (currentGuess.length > 0) {
+    // remove last letter from string
+    currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+
+    // calculate the cell index
+    const index = ANSWER_LENGTH * currentRow + currentGuess.length;
+
+    // clear the box
+    letters[index].innerText = "";
+  }
+}
+
+
+//res is response 
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const resObj = await res.json();
+  const word = resObj.word.toUpperCase();
+  const wordparts = word.split("");
+  setLoading(false);
+
+  console.log(word);
+   function addLetter (letter) {
+         if (currentGuess.length < ANSWER_LENGTH) {
             //add letters to the end
             currentGuess += letter;
          }else {
             //replace the last letter
             currentGuess = currentGuess.substring(0, currentGuess.length -1)+ letter;
-            
          }
          //show the text in screen 
-        
-       letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
-        
-     }
+          const index = currentRow * ANSWER_LENGTH + (currentGuess.length - 1);
 
-     function backspace() {
-     if (currentGuess.length > 0) {
-        // erase letter from the grid
-        const index = ANSWER_LENGTH * currentRow + (currentGuess.length - 1);
-        letters[index].innerText = "";
+          letters[index].innerText = letter;
+          }
 
-        // erase last letter
-        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
-      }
-     }
-
+              
 
      async function commit() {
         if (currentGuess.length !== ANSWER_LENGTH) {
             //do nothing 
             return;
+        }
+        const guespart = currentGuess.split("")
+        const workparts = word.split("");
+
+        for (let i = 0; i < ANSWER_LENGTH; i++) {
+            if (guespart[i] === wordparts[i]){
+                letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+            }
+            
         }
         //ToDo did they win or lose?
         currentRow++;
@@ -45,7 +69,7 @@ async function init() {
      document.addEventListener('keydown', function handleKeyPress (event){
         const action = event.key;
 
-        console.log(action);
+        
 
         if (action === 'Enter'){
             commit();//it used to gueses
@@ -61,4 +85,21 @@ async function init() {
  function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
  }
+    
+    /*
+   function setLoding(isLoding) {
+    if (isLoding) {
+        //show the loading
+        loadingDiv.classList.remove("hidden");
+    }else {
+        //hide the loading
+        loadingDiv.classList.add("hidden")
+        console.log("setLoading: HIDE");
+    }
+   }
+    */
+   // i am not sure about this code
+   function setLoading(isLoading) {
+    loadingDiv.classList.toggle('hidden', !isLoading)
+   }
 init();
